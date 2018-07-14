1ec008e87c88652f5464732c330f4309d8fcb9cc
Olegas Goncarovas <ogix777@gmail.com>
2018-07-14 12:37:40 +0300

    Export more types to the public api

--- projects/image-uploader/src/public_api.ts
+++ projects/image-uploader/src/public_api.ts
@@ -5,3 +5,5 @@
 export * from './lib/image-uploader.service';
 export * from './lib/image-uploader.component';
 export * from './lib/image-uploader.module';
+export * from './lib/interfaces';
+export * from './lib/uploaded-file';
