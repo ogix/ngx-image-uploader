ceea09a26483a560e93c620d80f95bdfbb447112
Olegas Goncarovas <ogix777@gmail.com>
2018-07-20 10:34:50 +0300

    Update demo app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { ImageUploaderOptions, ImageUploaderComponent } from 'image-uploader';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  imageOptions: ImageUploaderOptions = {
+    uploadUrl: 'https://api.pulseapp.eu/api/admin/news/uploadLogo',
+    cropEnabled: true,
+    thumbnailResizeMode: 'fill',
+    autoUpload: false,
+    resizeOnLoad: false,
+    thumbnailWidth: 320,
+    thumbnailHeight: 200
+  };
+
+  @ViewChild(ImageUploaderComponent) imageUploader: ImageUploaderComponent;
+
+  ngAfterViewInit() {
+    this.imageUploader.loadAndResize('https://pulsemediax.azureedge.net/news/64a9125e-c324-464f-8c4a-d5606bf2a0b6.jpg?width=512');
+  }
 }
