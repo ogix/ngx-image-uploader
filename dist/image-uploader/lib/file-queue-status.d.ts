49ebdeb6461e6795af50af8de759587645f6d090
Olegas Goncarovas <ogix777@gmail.com>
2018-07-22 01:26:26 +0300

    Revert "Include dist folder to make demo app work in StackBlitz"
    This reverts commit 31d892618acedd0c04a198ea6d9500ec523fbaa2.

--- dist/image-uploader/lib/file-queue-status.d.ts
+++
@@ -1,6 +0,0 @@
-export declare enum FileQueueStatus {
-    Pending = 0,
-    Success = 1,
-    Error = 2,
-    Progress = 3,
-}
--- dist/image-uploader/lib/utils.d.ts
+++
@@ -1,3 +0,0 @@
-import { ResizeOptions } from './interfaces';
-export declare function createImage(url: string, cb: (i: HTMLImageElement) => void): void;
-export declare function resizeImage(origImage: HTMLImageElement, {resizeHeight, resizeWidth, resizeQuality, resizeType, resizeMode}?: ResizeOptions): string;
